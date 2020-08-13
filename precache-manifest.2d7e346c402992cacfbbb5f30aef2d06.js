84084060add93f436500b5831127711f2e8bd358
Think Link <software@thinklink.com.co>
2020-08-12 21:34:32 -0500

    Updates

---
+++ precache-manifest.2d7e346c402992cacfbbb5f30aef2d06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a993a5f3344e6a069d28f04bd48e911",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef633ebf67cd5f60657d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4f4061bf96fef7a81ee9",
+    "url": "/static/js/2.899c24ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.899c24ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef633ebf67cd5f60657d",
+    "url": "/static/js/main.5edd4fde.chunk.js"
+  },
+  {
+    "revision": "1c498be53f139d2854b7",
+    "url": "/static/js/runtime-main.dbbc4393.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
